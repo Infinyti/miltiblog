63253d127c27494e18f020573a828ae3409a50b9
user <user@user.com>
2017-05-12 17:35:11 +0300

    e

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,144 @@
 @extends('layouts.app')
+@section('home')
+            <!-- main blog area start-->
+            <div class="main-blog-area ptb100 fix">
+               <div class="container">
+                   <div class="row">
+                       <div class="col-md-8 col-sm-7 col-xs-12">
+                           <div class="blog-body">
+                               <div class="row">
+                                    <div class="col-md-6 col-sm-12 col-xs-12">
+                                        <div class="single-news single-news-02"> 
+                                            <div class="news-img animate-eff">
+                                                <a href="#"><img src="img/blog/blog-03.jpg" alt=""></a>
+                                            </div>
+                                            <div class="news-contant">
+                                                <h3 class="uppercase"><a href="#">Design & Develop concept fot new Designer</a></h3>
+                                                <p>Post-by :<a href="#"> Admin</a> </p>
+                                                <div class="news-bottom shadow-box">
+                                                    <a href="#"><i class="zmdi zmdi-favorite-outline"></i>7 Like</a>
+                                                    <a href="#"><i class="zmdi zmdi-comment-text"></i>9 Comment</a>
+                                                    <a class="read-more" href="single-blog.html">Read More</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 col-sm-12 col-xs-12">
+                                        <div class="single-news single-news-02"> 
+                                            <div class="news-img animate-eff">
+                                                <a href="#"><img src="img/blog/blog-04.jpg" alt=""></a>
+                                            </div>
+                                            <div class="news-contant">
+                                                <h3 class="uppercase"><a href="#">Design & Develop concept fot new Designer</a></h3>
+                                                <p>Post-by :<a href="#"> Admin</a> </p>
+                                                <div class="news-bottom shadow-box">
+                                                    <a href="#"><i class="zmdi zmdi-favorite-outline"></i>7 Like</a>
+                                                    <a href="#"><i class="zmdi zmdi-comment-text"></i>9 Comment</a>
+                                                    <a class="read-more" href="single-blog.html">Read More</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 col-sm-12 col-xs-12">
+                                        <div class="single-news single-news-02"> 
+                                            <div class="news-img animate-eff">
+                                                <a href="#"><img src="img/blog/blog-05.jpg" alt=""></a>
+                                            </div>
+                                            <div class="news-contant">
+                                                <h3 class="uppercase"><a href="#">Design & Develop concept fot new Designer</a></h3>
+                                                <p>Post-by :<a href="#"> Admin</a> </p>
+                                                <div class="news-bottom shadow-box">
+                                                    <a href="#"><i class="zmdi zmdi-favorite-outline"></i>7 Like</a>
+                                                    <a href="#"><i class="zmdi zmdi-comment-text"></i>9 Comment</a>
+                                                    <a class="read-more" href="single-blog.html">Read More</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 col-sm-12 col-xs-12">
+                                        <div class="single-news single-news-02"> 
+                                            <div class="news-img animate-eff">
+                                                <a href="#"><img src="img/blog/blog-06.jpg" alt=""></a>
+                                            </div>
+                                            <div class="news-contant">
+                                                <h3 class="uppercase"><a href="#">Design & Develop concept fot new Designer</a></h3>
+                                                <p>Post-by :<a href="#"> Admin</a> </p>
+                                                <div class="news-bottom shadow-box">
+                                                    <a href="#"><i class="zmdi zmdi-favorite-outline"></i>7 Like</a>
+                                                    <a href="#"><i class="zmdi zmdi-comment-text"></i>9 Comment</a>
+                                                    <a class="read-more" href="single-blog.html">Read More</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 col-sm-12 col-xs-12">
+                                        <div class="single-news single-news-02"> 
+                                            <div class="news-img animate-eff">
+                                                <a href="#"><img src="img/blog/blog-07.jpg" alt=""></a>
+                                            </div>
+                                            <div class="news-contant">
+                                                <h3 class="uppercase"><a href="#">Design & Develop concept fot new Designer</a></h3>
+                                                <p>Post-by :<a href="#"> Admin</a> </p>
+                                                <div class="news-bottom shadow-box">
+                                                    <a href="#"><i class="zmdi zmdi-favorite-outline"></i>7 Like</a>
+                                                    <a href="#"><i class="zmdi zmdi-comment-text"></i>9 Comment</a>
+                                                    <a class="read-more" href="single-blog.html">Read More</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 col-sm-12 col-xs-12">
+                                        <div class="single-news single-news-02"> 
+                                            <div class="news-img animate-eff">
+                                                <a href="#"><img src="img/blog/news-01.jpg" alt=""></a>
+                                            </div>
+                                            <div class="news-contant">
+                                                <h3 class="uppercase"><a href="#">Design & Develop concept fot new Designer</a></h3>
+                                                <p>Post-by :<a href="#"> Admin</a> </p>
+                                                <div class="news-bottom shadow-box">
+                                                    <a href="#"><i class="zmdi zmdi-favorite-outline"></i>7 Like</a>
+                                                    <a href="#"><i class="zmdi zmdi-comment-text"></i>9 Comment</a>
+                                                    <a class="read-more" href="single-blog.html">Read More</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
+    
+		
+		<!--==== all js here====-->
+		<!-- jquery latest version -->
+        <script src="js/vendor/jquery-3.1.1.min.js"></script>
+		<!-- bootstrap js -->
+        <script src="js/bootstrap.min.js"></script>
+		<!-- owl.carousel js -->
+        <script src="js/owl.carousel.min.js"></script>
+		<!-- meanmenu js -->
+        <script src="js/jquery.meanmenu.js"></script>
+		<!-- Nivo js -->
+        <script src="js/nivo-slider/jquery.nivo.slider.pack.js"></script>
+        <script src="js/nivo-slider/nivo-active.js"></script>
+		<!-- wow js -->
+        <script src="js/wow.min.js"></script>
+        <!-- Youtube Background JS -->
+        <script src="js/jquery.mb.YTPlayer.min.js"></script>
+		<!-- datepicker js -->
+        <script src="js/bootstrap-datepicker.js"></script>
+		<!-- waypoint js -->
+        <script src="js/waypoints.min.js"></script>
+		<!-- onepage nav js -->
+        <script src="js/jquery.nav.js"></script>
+        <!-- animate text JS -->
+        <script src="js/animate-text.js"></script>
+		<!-- plugins js -->
+        <script src="js/plugins.js"></script>
+        <!-- main js -->
+        <script src="js/main.js"></script>
+@endsection
+
+
