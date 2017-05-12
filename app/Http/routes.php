a913fe1a76eec5e0db1d89d18fbc5ef66ff8a46f
introqt <introtheintro@gmail.com>
2017-05-12 17:24:58 +0300

    qwer

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,13 +1,20 @@
 <?php
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> 2897ababb9d299e86589f64c65a5633a442e15bf
 Route::get('/', function ($id = NULL) {
+=======
+Route::get('/', function () {
+>>>>>>> 706e9399d5b5d4303e29e5c54ef54f6a2632b3b5
     return view('home');
 });
 
+Route::get('/post', function ($id = NULL) {
+    return view('post');
+});
 
 Route::get('/categories', function ($id = NULL) {
     return view('categories');
