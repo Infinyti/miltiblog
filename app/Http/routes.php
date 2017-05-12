cfed94469cb68559fd3b4c7c1f7cb6e362fd9630
introqt <introtheintro@gmail.com>
2017-05-12 17:19:13 +0300

    save

---
+++ app/Http/routes.php
@@ -0,0 +1,27 @@
+<?php
+
+<<<<<<< HEAD
+
+=======
+>>>>>>> 2897ababb9d299e86589f64c65a5633a442e15bf
+Route::get('/', function ($id = NULL) {
+    return view('home');
+});
+
+
+Route::get('/categories', function ($id = NULL) {
+    return view('categories');
+});
+
+Route::get('/authors', function () {
+    return view('authors');
+});
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
