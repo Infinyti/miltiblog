83b83ea52a1cf2d0185fbb20047b9db83b97bd1e
dimafil1903 <dimafilipenko2002@gmail.com>
2017-05-14 09:20:51 +0300

    sorry

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,7 +18,7 @@ Route::delete('/admin/post/del/{post}', 'AdminPostController@del');
 Route::get('/admin/cat', 'AdminCatController@index');
 Route::post('/admin/cat', 'AdminCatController@add');
 Route::delete('/admin/cat/del/{cat}', 'AdminCatController@del');
-
+Route::post('/admin/cat/update/{cat}', 'AdminCatController@update');
 // Authentication routes...
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
--- resources/views/cat_govern.blade.php
+++ resources/views/cat_govern.blade.php
@@ -15,14 +15,28 @@
 </form>
 <br><br>
 <table>
-@foreach($cats as $cat)
-<tr>
-    <td class="table-text">
-        <div>{{ $cat->name }}</div>
-    </td>
-    <td class="table-text">
-        <div>{{ $cat->description }}</div>
-    </td>
+    @foreach($cats as $cat)
+    <tr>
+    <form action="{{ url('admin/cat/update/'.$cat->id) }}" method="POST">
+        {{ csrf_field() }}
+        {{ method_field('POST') }}
+
+
+        <td class="table-text">
+            <div>{{ $cat->name }}</div>
+            <input class="hide" type="text" value="{{ $cat->name }}" id="newname" name="newname"><br>
+
+        </td>
+        <td class="table-text">
+            <input type="hidden" value="{{ $cat->id }}" name="id"><br>
+            <div>{{ $cat->description }}</div>
+            <textarea class="hide"  value="" id="newdescription" name="newdescription">{{ $cat->description }}</textarea><br>
+            <button type="submit" id="save" class="btn hide ">
+                <i class="fa fa-"></i> сохранить
+            </button>
+        </td>
+
+    </form>
     <td>
         <form action="{{ url('admin/cat/del/'.$cat->id) }}" method="POST">
             {{ csrf_field() }}
@@ -32,8 +46,14 @@
                 <i class="fa fa-trash"></i> Удалить
             </button>
         </form>
+
+
+
+        <input type="button" class="hider" value="edit">
+
     </td>
 </tr>
 @endforeach
 </table>
 @endsection
+
